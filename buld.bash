7186de8dd8a6dfdd013d4fa8f7406de0244d92c2
Joseph Turner <Tearran@users.noreply.github.com>
2022-04-29 01:40:55 -0700

    Update buld.bash

--- buld.bash
+++ buld.bash
@@ -1,20 +1,22 @@
-
-#!/usr/bin/bash
 #!/bin/bash
-
 {
-cd /tmp/
+cd /tmp/ || ( echo "what not /tmp/" && exit )
 buildname="build"
 execName="iic-scanner"
 
 mkdir $buildname
-cd ./$buildname
-wget https://raw.githubusercontent.com/muhammadrefa/python-i2c-scanner/master/i2c-scanner.py -O __main__.py
+cd ./$buildname || ( echo "faild to make $buildname" && exit )
+
+wget https://raw.githubusercontent.com/muhammadrefa/python-i2c-scanner/master/i2c-scanner.py
+sed 's|print("I2C|#print("I2C|'  i2c-scanner > __main__.py
+sed -i 's|print("Start|#print("Start|'  __main__.py
+sed -i 's|print("End|#print("End|'  __main__.py
+sed -i 's|print(hex(i) + " -> " + res)|print(hex(i) + ", ")|' __main__
+
 wget https://raw.githubusercontent.com/kplindegaard/smbus2/master/smbus2/smbus2.py
 zip -r ../$buildname.zip * 
 cd ..
 echo '#!/usr/bin/env python' | cat - $buildname.zip > $execName
-chmod +x $execName
-cd /tmp/
+sudo install -b -v $execName /usr/bin/
 
 }
