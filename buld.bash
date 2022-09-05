e2635531cea0e90b3655b14382e5a260973c34a7
Joseph Turner <Tearran@users.noreply.github.com>
2022-09-05 10:59:22 -0700

    Update buld.bash

--- buld.bash
+++ buld.bash
@@ -4,20 +4,19 @@
 # Change to /tmp/ directory to build zip
 cd /tmp/ || ( echo "what not /tmp/" && exit )
 # naming conviance 
-buildname="build"
-execName="iic-scanner"
+buildname="build_its-i2cScan"
+execName="its-i2cScan"
 
 #make a build folder
 mkdir $buildname
 # goto build folder
 cd ./$buildname || ( echo "faild to make $buildname" && exit )
 # download source iis scaner
-wget https://raw.githubusercontent.com/muhammadrefa/python-i2c-scanner/master/i2c-scanner.py
-# comment out unwanted print statments
-cp i2c-scanner.py  __main__.py
+wget https://raw.githubusercontent.com/Tearran/its-i2cScan/main/__main__.py
 
 #download smbus2 dropin lib
 wget https://raw.githubusercontent.com/kplindegaard/smbus2/master/smbus2/smbus2.py
+
 # compress the contents to a zip
 zip -r ../$buildname.zip ./* 
 cd ..
@@ -25,11 +24,5 @@ cd ..
 echo '#!/usr/bin/env python3' | cat - $buildname.zip > $execName
 # install the exicutible
 sudo install -b -v $execName /usr/bin/
-
-# type iic-scanner <dev#>
-# example
-# $iic-scanner 1   
-# to uninstall iic-scanner
-# $ sudo rm /user/bin/iic-scanner
 
 }
